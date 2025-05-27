2784a7691302e0781fee2475b1ec221b450b1081
german <germanlujan2018@gmail.com>
2025-05-27 16:28:27 -0300

    correciones vista

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Publicacion
 
 def lista_public(request): 
-    pubkucaciones=publicacion.objects.filter
-    (fecha_publicacion_lte=timezone.now()).
-    order_by('fecha_publicacion')
+
+    publicaciones=Publicacion.objects.filter(fecha_publicacion_lte=timezone.now()).order_by('-fecha_publicacion')
     return render(request, 'blog/lista_public.html', {'publicaciones':publicaciones})
 # Create your views here.
