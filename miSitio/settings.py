1c7de482a113d2a0113614e42ff81a4319f0a8eb
german <germanlujan2018@gmail.com>
2025-06-14 23:00:26 -0300

    cambios settings

--- miSitio/settings.py
+++ miSitio/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'miSitio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'blog2/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
